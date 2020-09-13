b5862145095076adf711dd48bb1a1f6698a4af8c
MT <1163182175@qq.com>
2020-09-13 16:43:18 +0800

    调整内容，更新静态页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <div>查看源码：</div>
+    <a style="color:blue" href="https://github.com/MissThee/vue-element-sample">https://github.com/MissThee/vue-element-sample</a>
   </div>
 </template>
 
